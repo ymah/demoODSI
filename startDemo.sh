3183e03bcf2bbbc225f2b6197541af3bbc97ffa1
Mahmoud Morsy <mahmoud.morsy.a@gmail.com>
2018-07-30 11:15:27 +0200

    add || exist after each make

--- startDemo.sh
+++ startDemo.sh
@@ -16,23 +16,23 @@ fi
 
 if [ "$1" == "galileo" ]
 then
-    make -C libpip/ VARIANT=galileo clean all
+    make -C libpip/ VARIANT=galileo clean all || exit
 else
-    make -C libpip/ clean all
+    make -C libpip/ clean all || exit
 fi
 
-make -C libfreertos clean all
+make -C libfreertos clean all || exit
 
 echo "Owner" && sleep 1
-make -C pipcore-mp/src/partitions/x86/owner/ all
+make -C pipcore-mp/src/partitions/x86/owner/ all || exit
 echo "sp1" && sleep 1
-make -C pipcore-mp/src/partitions/x86/sp1Task/ all
+make -C pipcore-mp/src/partitions/x86/sp1Task/ all || exit
 echo "sp2" && sleep 1
-make -C pipcore-mp/src/partitions/x86/sp2Task/ all
+make -C pipcore-mp/src/partitions/x86/sp2Task/ all || exit
 echo "sp3" && sleep 1
-make -C pipcore-mp/src/partitions/x86/sp3Task/ all
+make -C pipcore-mp/src/partitions/x86/sp3Task/ all || exit
 echo "Network manager" && sleep 1
-make -C pipcore-mp/src/partitions/x86/NetworkMngr/ all
+make -C pipcore-mp/src/partitions/x86/NetworkMngr/ all || exit
 echo "Compilation des sous partition terminée" && sleep 1
 
 yes | cp pipcore-mp/src/partitions/x86/owner/pip-freertos.bin pipcore-mp/src/partitions/x86/pip-freertos/Demo/pip-kernel/Support_Files/partitions_images/part1.bin
@@ -43,11 +43,11 @@ yes | cp pipcore-mp/src/partitions/x86/NetworkMngr/pip-freertos.bin pipcore-mp/s
 
 
 #make -C pipcore-mp/src/partitions/x86/pip-freertos/Demo/pip-kernel/ clean all
-make -B -C pipcore-mp/src/partitions/x86/pip-freertos/ all
+make -B -C pipcore-mp/src/partitions/x86/pip-freertos/ all || exit
 
 if [ "$1" == "galileo" ]
 then
-    make -C pipcore-mp TARGET=galileo PARTITION=pip-freertos clean partition kernel
+    make -C pipcore-mp TARGET=galileo PARTITION=pip-freertos clean partition kernel || exit
 else
-    make -C pipcore-mp TARGET=x86_multiboot PARTITION=pip-freertos clean partition kernel
+    make -C pipcore-mp TARGET=x86_multiboot PARTITION=pip-freertos clean partition kernel || exit
 fi
